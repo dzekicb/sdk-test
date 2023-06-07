386a8de6b5edf941bc8b3d879a78536b5a034e29
Michael Standen <screaminghawk@gmail.com>
2023-06-08 09:47:01 +1200

    Complete example

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-import { Tenderly, Network } from '@tenderly/sdk';
-
+import { Tenderly, Network } from "@tenderly/sdk";
 
 const libraryTokenContract =
-  '0x56AEE36ECc39e8eC258d36c39c504D12A6EA2091'.toLowerCase();
+  "0x784aDFBdee81Bce425224fBc269295DD4Bc2b019".toLowerCase();
 
 (async () => {
   try {
@@ -13,33 +12,46 @@ const libraryTokenContract =
       network: Network.SEPOLIA, // Replace with the appropriate network
     });
     const result = await tenderly.contracts.verify(libraryTokenContract, {
-      "config": {
-        "mode": "public"
+      config: {
+        mode: "public",
       },
-      "contractToVerify": "contracts/NamedCounter.sol:NamedCounter",
-      "solc": {
-        "version": "v0.8.18",
-        "sources": {
+      contractToVerify: "contracts/NamedCounter.sol:NamedCounter",
+      solc: {
+        version: "v0.8.18",
+        sources: {
           "contracts/NamedCounter.sol": {
-            "content": "// SPDX-License-Identifier: MIT\npragma solidity ^0.8.18;\n\nimport {CounterWithLogs} from \"remap-me/Counter/Counter.sol\";\n\ncontract NamedCounter is CounterWithLogs {\n  string public name;\n\n  function setName(string memory _name) public {\n    name = _name;\n  }\n}\n"
+            content:
+              '// SPDX-License-Identifier: MIT\npragma solidity ^0.8.18;\n\nimport {CounterWithLogs} from "remap-me/Counter/Counter.sol";\n\ncontract NamedCounter is CounterWithLogs {\n  string public name;\n\n  function setName(string memory _name) public {\n    name = _name;\n  }\n}\n',
           },
           "remapped/Counter/Counter.sol": {
-            "content": "// SPDX-License-Identifier: MIT\npragma solidity ^0.8.18;\ncontract CounterWithLogs {\n  uint public count;\n  event CounterChanged(\n    string method,\n    uint256 oldNumber,\n    uint256 newNumber,\n    address caller\n  );\n  // Function to get the current count\n  function get() public view returns (uint) {\n    return count;\n  }\n  // Function to increment count by 1\n  function inc() public {\n    emit CounterChanged(\"Increment\", count, count + 1, msg.sender);\n    count += 1;\n  }\n  // Function to decrement count by 1\n  function dec() public {\n    emit CounterChanged(\"Decrement\", count, count - 1, msg.sender);\n    count -= 1;\n  }\n}\n"
-          }
+            content:
+              '// SPDX-License-Identifier: MIT\npragma solidity ^0.8.18;\ncontract CounterWithLogs {\n  uint public count;\n  event CounterChanged(\n    string method,\n    uint256 oldNumber,\n    uint256 newNumber,\n    address caller\n  );\n  // Function to get the current count\n  function get() public view returns (uint) {\n    return count;\n  }\n  // Function to increment count by 1\n  function inc() public {\n    emit CounterChanged("Increment", count, count + 1, msg.sender);\n    count += 1;\n  }\n  // Function to decrement count by 1\n  function dec() public {\n    emit CounterChanged("Decrement", count, count - 1, msg.sender);\n    count -= 1;\n  }\n}\n',
+          },
         },
-        "settings": {
-          "optimizer": {
-            "enabled": true,
-            "runs": 20000,
-            // Not supported
-            // "remappings": [
-            //   "remap-me/Counter/=remapped/Counter/"
-            // ],
-          }
-        }
-      }
+        settings: {
+          viaIR: true,
+          optimizer: { enabled: true, runs: 20000 },
+          outputSelection: {
+            "*": {
+              "*": [
+                "abi",
+                "evm.bytecode",
+                "evm.deployedBytecode",
+                "evm.methodIdentifiers",
+                "metadata",
+              ],
+              "": ["ast"],
+            },
+          },
+          remappings: [
+            "ds-test/=lib/forge-std/lib/ds-test/src/",
+            "forge-std/=lib/forge-std/src/",
+            "remap-me/Counter/=remapped/Counter/",
+          ],
+        },
+      },
     });
-    console.log('Result:', result);
+    console.log("Result:", result);
   } catch (error) {
     console.error(error);
   }
